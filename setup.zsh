53f3453af666c9d88e7b7a50a49dee2b5d6f95fd
drupallerina <drupallerina@gmail.com>
2015-07-11 22:53:25 +0200

    add alias for vagrant, add directories on initial setup

--- setup.zsh
+++ setup.zsh
@@ -3,7 +3,9 @@
 # setup script kindly borrowed from dfm and adjusted for our purposes as alternative to rake to augment fresh
 
 # make bin directory in home if it doesn't already exist
-mkdir -p ~/bin
+[ ! -d $HOME/bin ] && mkdir -p $HOME/bin
+# make terminator config direcotry if it doesn't already exist
+[ ! -d $HOME/.config/terminator ] && mkdir -p $HOME/.config/terminator
 
 # setup fresh
 #git clone https://github.com/freshshell/fresh.git ~/.fresh/source/freshshell/fresh
