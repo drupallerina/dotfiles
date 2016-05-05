8917addbecadd1511037b1866585de5ca4d3b57c
drupallerina <drupallerina@gmail.com>
2016-05-05 23:44:01 +0200

    drush8 alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,4 +5,4 @@ alias git-team="git log --format='%aN' | sort -u"
 # drush
 alias drush7='~/drush7/vendor/bin/drush'
 alias drush8='~/drush8/vendor/bin/drush'
-alias drush=drush8
+alias drush='drush8'
