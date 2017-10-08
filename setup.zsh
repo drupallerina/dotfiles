d5e92b67fc8ede84e73bb72da8a30399f3239d76
drupallerina <drupallerina@1171752.no-reply.drupal.org>
2017-10-08 21:54:14 +0200

    Clean up setup.zsh.

--- setup.zsh
+++ setup.zsh
@@ -13,33 +13,9 @@
 #~/.fresh/source/freshshell/fresh/bin/fresh
 FRESH_LOCAL_SOURCE=drupallerina/dotfiles bash <(curl -sL get.freshshell.com)
 
-
-# setup vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-
-# setup cheat
-pip install git+git://github.com/chrisallenlane/cheat.git
-
-# setup prezto - this is done by fresh
-#git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-#setopt EXTENDED_GLOB
-#for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-#ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-#done
-
 # Install atom plugins
-#apm install --packages-file ~/.dotfiles/config/atom/atom-packages.json
-
-# import terminal profiles
-#gconftool-2 --load ~/.fresh/build/gnome-terminal-conf.xml
+apm install --packages-file ~/.config/friendlyarch/defaults/apm.list
 
 # switch to zsh
 which zsh
 chsh -s $(which zsh)
-
-# install xiki
-#cd ~/.src/xiki
-#gem install bundler
-#bundle
-#ruby etc/command/copy_xiki_command_to.rb /usr/local/bin/xiki
-#cd
