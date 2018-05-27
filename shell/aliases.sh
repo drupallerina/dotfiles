770ac2ac83c09b670216d7dc55c43c40e612384e
drupallerina <nika@acolono.com>
2018-05-27 19:18:09 +0200

    Remove unused aliases.

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,10 +1,4 @@
-# vagrant
-alias vg='vagrant'
 # git
 alias git-team="git log --format='%aN' | sort -u"
-# drush
-#alias drush7='~/drush7/vendor/bin/drush'
-#alias drush8='~/drush8/vendor/bin/drush'
-#alias drush='drush8'
-# other
+# atom
 alias a='atom'
